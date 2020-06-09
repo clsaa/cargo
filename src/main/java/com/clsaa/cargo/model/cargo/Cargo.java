cca20a6a9a5087b8a20cddaee4cd1a9c34e4eeae
clsaa <812022339@qq.com>
2020-06-09 14:49:23 +0800

    part

--- src/main/java/com/clsaa/cargo/model/cargo/Cargo.java
+++ src/main/java/com/clsaa/cargo/model/cargo/Cargo.java
@@ -1,12 +1,14 @@
 package com.clsaa.cargo.model.cargo;
 
 import com.clsaa.cargo.annotation.AggregateRoot;
+import com.clsaa.cargo.annotation.DefinesIdentity;
 import com.clsaa.cargo.annotation.Entity;
 import com.clsaa.cargo.model.customer.CustomerRepository;
 
 @AggregateRoot
 @Entity(root = Cargo.class)
 public class Cargo {
+    @DefinesIdentity
     private String trackingId;
     private DeliverySpecification deliverySpecification;
     private Itinerary itinerary;
